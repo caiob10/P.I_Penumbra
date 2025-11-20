7826a299c960435f1ba75124c9c2e77c0dc5594f
caiob10 <caiojosequeiroz49@gmail.com>
2025-11-20 20:52:57 -0300

    Caio-20-11

--- Assets/Scripts/Utilities/win.cs
+++ Assets/Scripts/Utilities/win.cs
@@ -6,12 +6,18 @@ public class win : MonoBehaviour
     public Raiva_status raiva;
     public Enemy_Vergonha vergonha;
     public Medo_Vida medo;
+    private float tempo = 0f;
     
     void Update()
     {
         if (raiva.raivaMorto && vergonha.vergonhaMorto && medo.medoMorto)
         {
-            SceneManager.LoadScene("win");
+            tempo += Time.deltaTime;
+            if (tempo >= 2)
+            {
+                SceneManager.LoadScene("win");
+            }
+            
         }
 
 
